42c2f88cfa9fbc2401dd25d96eeb74f80c233fec
batwood99 <atwood@mail.usf.edu>
2023-07-16 10:39:01 -0400

    added asset caching in src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,8 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+    request.destination === 'script' || request.destination === 'style' || request.destination === 'font',
+  new CacheFirst()
+);
