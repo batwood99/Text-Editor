869049c4ae6538e1dfa959102adea1aa758a3971
batwood99 <atwood@mail.usf.edu>
2023-07-16 10:50:55 -0400

    webpack config first draft

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,25 +6,62 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      inject: true,
+      chunks: ['main'],
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      inject: true,
+      chunks: ['install'],
+      filename: 'install.html',
+    }),
+    new WebpackPwaManifest({
+      name: 'J.A.T.E',
+      short_name: 'JATE',
+      icons: [
+        {
+          src: path.resolve("./src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          type: "image/png",
+        },
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: './client/src-sw.js',
+      swDest: 'service-worker.js',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
